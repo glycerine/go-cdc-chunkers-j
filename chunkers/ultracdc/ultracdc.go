b975065d7d66c0f77b847b7c6448265f45b082d0
Jason E. Aten <j.e.aten@gmail.com>
2024-12-29 20:35:09 -0600

    ultracdc.go: get correctness, shed complexity.
       Fixes several issues that were preventing
       the UltraCDC implementation from giving
       correct answers. Adds some basic tests
       for the desired perservation of
       cutpoints and the hashes of their
       segments. Getting rid of the unsafe
       package is also nice for portability.

--- chunkers/ultracdc/ultracdc.go
+++ chunkers/ultracdc/ultracdc.go
@@ -17,9 +17,10 @@
 package ultracdc
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"math/bits"
-	"unsafe"
 
 	chunkers "github.com/PlakarKorp/go-cdc-chunkers"
 )
@@ -31,7 +32,6 @@ func init() {
 var ErrNormalSize = errors.New("NormalSize is required and must be 64B <= NormalSize <= 1GB")
 var ErrMinSize = errors.New("MinSize is required and must be 64B <= MinSize <= 1GB && MinSize < NormalSize")
 var ErrMaxSize = errors.New("MaxSize is required and must be 64B <= MaxSize <= 1GB && MaxSize > NormalSize")
-var ErrMinSizeNotMultipleOf8 = errors.New("MinSize must be evenly divisible by 8")
 
 type UltraCDC struct {
 }
@@ -49,9 +49,7 @@ func (c *UltraCDC) DefaultOptions() *chunkers.ChunkerOpts {
 }
 
 func (c *UltraCDC) Validate(options *chunkers.ChunkerOpts) error {
-	if options.MinSize%8 != 0 {
-		return ErrMinSizeNotMultipleOf8
-	}
+
 	if options.NormalSize == 0 || options.NormalSize < 64 ||
 		options.NormalSize > 1024*1024*1024 {
 		return ErrNormalSize
@@ -67,63 +65,139 @@ func (c *UltraCDC) Validate(options *chunkers.ChunkerOpts) error {
 	return nil
 }
 
-func (c *UltraCDC) Algorithm(options *chunkers.ChunkerOpts, data []byte, n int) int {
-	src := (*uint64)(unsafe.Pointer(&data[0]))
+// Algorithm's return value, cutpoint, might typically be used next in
+// segment := data[:cutpoint], so we expect to exclude the cutpoint
+// index value itself. Also commonly when n == len(data) and data is
+// short, then the returned cutpoint will be n;
+// n is the default to return when we did not find a shorter
+// cutpoint. The segment := data[:len(data)] will then take
+// all of data as the segment to hash.
+//
+// PRE condition: n must be <= len(data). We will panic if this does not hold.
+// It is always safe to pass n = len(data).
+//
+// POST INVARIANT: cutpoint <= n. We never return a cutpoint > n.
+func (c *UltraCDC) Algorithm(options *chunkers.ChunkerOpts, data []byte, n int) (cutpoint int) {
 
-	const (
-		Pattern uint64 = 0xAAAAAAAAAAAAAAAA
-		MaskS   uint64 = 0x2F
-		MaskL   uint64 = 0x2C
-		LEST    uint32 = 64
-	)
-	MinSize := options.MinSize
-	MaxSize := options.MaxSize
-	NormalSize := options.NormalSize
-
-	i := MinSize
-	cnt := uint32(0)
-	mask := MaskS
-
-	switch {
-	case n <= MinSize:
-		return n
-	case n >= MaxSize:
-		n = MaxSize
-	case n <= NormalSize:
-		NormalSize = n
+	// A common case will be n == len(data), but n could certainly be less.
+	// Confirm that it is never more.
+	if n > len(data) {
+		panic(fmt.Sprintf("len(data) == %v and n == %v: n must be <= len(data)", len(data), n))
 	}
 
-	outBufWin := (*uint64)(unsafe.Pointer(uintptr(unsafe.Pointer(src)) + uintptr(i)))
-	dist := uint64(bits.OnesCount64(*outBufWin ^ Pattern))
-	i += 8
+	const (
+		maskS uint64 = 0x2F // binary 101111
 
-	for i < n {
-		if i == NormalSize {
-			mask = MaskL
+		// maskL ignores 2 more bits than maskS, so
+		// it is easier to match (so we get a higher
+		// probability of match after the normal point).
+		maskL uint64 = 0x2C // binary 101100
+
+		lowEntropyStringThreshold int = 64 // LEST in the paper.
+	)
+	minSize := options.MinSize
+	maxSize := options.MaxSize
+	normalSize := options.NormalSize
+
+	var lowEntropyCount int
+
+	// initial mask for small cuts below the Normal point.
+	mask := maskS
+
+	switch {
+	case n <= minSize:
+		cutpoint = n
+		return
+	case n >= maxSize:
+		n = maxSize
+	case n <= normalSize:
+		normalSize = n
+	}
+
+	outBufWin := data[minSize : minSize+8]
+
+	// Initialize hamming distance on outBufWin
+	dist := 0
+	for _, v := range outBufWin {
+		// effectively the Pattern of 0xAAAAAAAAAAAAAAAA,
+		// as referenced in the paper,
+		// is expressed here, just one byte at a time.
+		dist += bits.OnesCount8(v ^ 0xAA)
+	}
+
+	var inBufWin []byte
+	for i := minSize + 8; i <= n-8; i += 8 {
+		if i >= normalSize {
+			// Yes, we write mask every time after the Normal point,
+			// and at first this appears wasteful. However,
+			// an engineering judgement call was made to keep it.
+			//
+			// The rationale is that this small redudancy
+			// is cheaper, simpler, and safer
+			// than duplicating all the logic below for the two different
+			// masks and then having to be sure to keep
+			// the duplicates in sync. We saw multiple bugs in the past from
+			// MinSize and NormalSize not being 8 byte aligned,
+			// and we'd also rather not impose that on users.
+			// The POST invariance analysis is also much simplified.
+			// The CPU has to do less branch prediction this way,
+			// and maskL will almost surely be quickly
+			// accessible in a cache line.
+			mask = maskL
 		}
 
-		inBufWin := (*uint64)(unsafe.Pointer(uintptr(unsafe.Pointer(src)) + uintptr(i)))
-		if (*outBufWin ^ *inBufWin) == 0 {
-			cnt++
-			if cnt == LEST {
-				return i + 8
+		// If i == n-8 then i+8 == n, and since n <= len(data)
+		// as a PRE condition, we never go out of bounds.
+		inBufWin = data[i : i+8]
+
+		if bytes.Equal(inBufWin, outBufWin) {
+			lowEntropyCount++
+			if lowEntropyCount >= lowEntropyStringThreshold {
+				// on random (high-entropy) data, we don't expect to get here.
+
+				// If i == n-8, its largest, then this returns n,
+				// which maintains our POST INVARIANT that cutpoint <= n.
+				cutpoint = i + 8
+				return
 			}
-			i += 8
 			continue
 		}
 
-		cnt = 0
+		lowEntropyCount = 0
 		for j := 0; j < 8; j++ {
-			if (dist & mask) == 0 {
-				return i + 8
+			if (uint64(dist) & mask) == 0 {
+				// Do we preserve the POST INVARIANT here?
+				// if i == n-8 (the biggest possible), and
+				// j is 7 (its biggest possible), then
+				// i + j could here be as big as n - 8 + 7 == n-1
+				// So, yes, n-1 is the biggest we could return here.
+				cutpoint = i + j
+				return
 			}
-			inByte := *(*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(inBufWin)) + uintptr(j)))
-			outByte := *(*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(outBufWin)) + uintptr(j)))
-			dist = dist + uint64(hammingDistanceTable[outByte][inByte])
+			outByte := data[i+j-8]
+			inByte := data[i+j]
+
+			// The hamming distance instruction POPCNT is
+			// typically available in today's hardware, but
+			// upon measurement the lookup table still looks
+			// faster; plus its more portable.
+			//
+			// I'll leave the bits.OnesCountXX (POPCNT based)
+			// version here in case newer hardware gets even faster; or maybe we
+			// weren't using the hardware right when we measured.
+			// Or maybe only bits.OnesCount64 uses POPCNT? Not worth
+			// going deeper at the moment.
+			//
+			// https://stackoverflow.com/questions/28802692/how-is-popcnt-implemented-in-hardware
+			//
+			//update := bits.OnesCount8(inByte^0xAA) - bits.OnesCount8(outByte^0xAA)
+			update := hammingDistanceTo0xAA[inByte] - hammingDistanceTo0xAA[outByte]
+			dist += update
 		}
 		outBufWin = inBufWin
-		i += 8
 	}
 
-	return n
+	// obviously preserves the POST INVARIANT that cutpoint <= n.
+	cutpoint = n
+	return
 }
